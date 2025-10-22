c2f280e0f8db2a97477b2bc380882c3972c0603c
Boxy <nozakisola@gmail.com>
2025-10-21 23:23:50 -0500

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
+	appType: 'spa',
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					preact: ['preact', 'preact/hooks', 'preact-iso'],
+					wiki: ['wikipedia']
+				}
+			}
+		}
+	},
 	plugins: [preact()],
 });
