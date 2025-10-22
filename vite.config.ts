3f0923b9f007a85858f77f0ed019c6dd20ab7113
Boxy <boxy@myyahoo.com>
2025-10-22 07:12:59 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
+	base: './HomePage',
 	appType: 'spa',
 	build: {
 		rollupOptions: {
