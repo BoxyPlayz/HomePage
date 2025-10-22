e182835a0e50a5b4259f27e081a6d5946e048374
Boxy <boxy@myyahoo.com>
2025-10-22 07:17:09 -0500

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './HomePage',
+	base: 'https://boxyplayz.github.io/HomePage/',
 	appType: 'spa',
 	build: {
 		rollupOptions: {
