0c5be07d28565a959b4ed1d73e21c39bc3f5959a
Boxy <nozakisola@gmail.com>
2025-10-22 21:39:43 -0500

    IF THIS DOSENT WORK IM GONNA-

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'preact';
 import { LocationProvider, Router, Route, lazy, ErrorBoundary } from 'preact-iso';
 import './style.css';
 import { Header } from '@/components/Header';
-import { addBase } from '@/utils';
+import { addBase, get_base_url } from '@/utils';
 
 const Home = lazy(() => import('./pages/Home/index'));
 const Settings = lazy(() => import('./pages/Settings/index'));
@@ -14,7 +14,7 @@ export function App() {
 			<ErrorBoundary>
 				<Header />
 				<Router>
-					<Route path={import.meta.env.BASE_URL || '/'} component={Home} />
+					<Route path={get_base_url()} component={Home} />
 					<Route path={addBase("settings")} component={Settings} />
 					<Route default component={NotFound} />
 				</Router>
