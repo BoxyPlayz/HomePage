382f033e5dcb3f2cc20e904cc37ee34967e53c5e
Boxy <nozakisola@gmail.com>
2025-12-23 16:55:12 -0600

    Make PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import preact from '@preact/preset-vite';
 import 'dotenv/config';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,5 +28,29 @@ export default defineConfig({
 	resolve: { alias: { '@/': '/src/' } },
 	server: { port: 8000 },
 	preview: { port: 8000 },
-	plugins: [preact(), nodePolyfills({ include: ['path'] })],
+	plugins: [
+		preact(),
+		nodePolyfills({ include: ['path'] }),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			pwaAssets: { disabled: false, config: true },
+			manifest: {
+				name: "Boxy's Homepage",
+				short_name: 'Searchy',
+				description: 'A goofy little homepage I made.',
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico,txt,json}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module',
+			},
+		}),
+	],
 });
